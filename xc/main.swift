fce50d0324469565dfb40277094d98e5a6cf14fb
Vazquez, Jose <Jose.Vazquez2@capitalone.com>
2016-05-21 19:46:53 -0400

    Initial Commit

---
+++ xc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xc
+//
+//  Created by Vazquez, Jose on 5/21/16.
+//  Copyright © 2016 Capital One Financial. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
