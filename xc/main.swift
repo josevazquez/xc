6b5009fabf04bd14cc33b04117d4f3848dd39a80
Vazquez, Jose <Jose.Vazquez2@capitalone.com>
2016-09-10 09:39:44 -0400

    Added ability to open any file that matches a given extension. with this, we can now open any found workspace or xcodeproj. (also updated to Swift 3)

--- xc/main.swift
+++ xc/main.swift
@@ -5,16 +5,16 @@ import AppKit
 let Xcode = "Xcode"
 
 // Get all arguments and remove the first one which is the name of the executable itself.
-var arguments = Process.arguments
-arguments.removeAtIndex(0)
+var arguments = CommandLine.arguments
+arguments.remove(at: 0)
 
 
 // Get current directory path and assigne the last element of the path to the projectFolder
-let fileManager = NSFileManager.defaultManager()
+let fileManager = FileManager.default
 let path = fileManager.currentDirectoryPath
-let url = NSURL(fileURLWithPath: path)
+let url = URL(fileURLWithPath: path)
 
-guard let projectFolder = url.pathComponents?.last else {
+guard let projectFolder = url.pathComponents.last else {
     print("Somehow this tool was executed with no PWD.")
     exit(EXIT_FAILURE)
 }
@@ -23,13 +23,36 @@ guard let projectFolder = url.pathComponents?.last else {
 let workspacePath = "\(path)/\(projectFolder).xcworkspace"
 let projectPath = "\(path)/\(projectFolder).xcodeproj"
 
-// Helper function of open up files if they exist. can optionally exit the script on success.
-func openFile(fullPath:String, withApplication application:String, exitOnSuccess:Bool) -> Bool {
-    let success = NSWorkspace.sharedWorkspace().openFile(fullPath, withApplication:application)
+/// Helper function of open up files if they exist. can optionally exit the script on success.
+func open(file:String, application:String, exitOnSuccess:Bool) -> Bool {
+    let success = NSWorkspace.shared().openFile(file, withApplication:application)
     if exitOnSuccess && success {
         exit(EXIT_SUCCESS)
     }
     return success
+}
+
+/// Helper function that opens any files that end with the passed extension. If
+/// any matching files are found, we exit execution.
+func open(filesWithExtension fileExtension:String) {
+    var files = [String]()
+
+    do {
+        files = try fileManager.contentsOfDirectory(atPath: path)
+    } catch {
+        print("Failed to read contents of current directory: \(error)")
+        NSWorkspace.shared().launchApplication(Xcode)
+    }
+
+    var fileFound = false
+    for file in files.filter({$0.hasSuffix(fileExtension)}) {
+        let fullPath = "\(path)/\(file)"
+        fileFound = fileFound || open(file:fullPath, application:Xcode, exitOnSuccess:false)
+    }
+
+    if(fileFound) {
+        exit(EXIT_SUCCESS)
+    }
 }
 
 
@@ -40,19 +63,23 @@ if arguments.count > 0 {
         // Try it again as an absolute path.
         let relativeFilePath = "\(path)/\(file)"
 
-        if openFile(relativeFilePath, withApplication:Xcode, exitOnSuccess:false) == false {
-            openFile(file, withApplication:Xcode, exitOnSuccess:false)
+        if open(file:relativeFilePath, application:Xcode, exitOnSuccess:false) == false {
+            _ = open(file:file, application:Xcode, exitOnSuccess:false)
         }
     }
 
 // If not arguments were passed open Xcode with some sensical default.
 } else {
     // Try to open the workspace first, and quit if success.
-    openFile(workspacePath, withApplication:Xcode, exitOnSuccess:true)
+    _ = open(file:workspacePath, application:Xcode, exitOnSuccess:true)
 
     // Now, try to open the project, and quit if that succeeds.
-    openFile(projectPath, withApplication:Xcode, exitOnSuccess:true)
+    _ = open(file:projectPath, application:Xcode, exitOnSuccess:true)
+
+    open(filesWithExtension:".xcworkspace")
+    open(filesWithExtension:".xcodeproj")
 
     // Finally, just attempt to launch Xcode.
-    NSWorkspace.sharedWorkspace().launchApplication(Xcode)
+    NSWorkspace.shared().launchApplication(Xcode)
 }
+
